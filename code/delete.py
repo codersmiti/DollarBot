1331a61c69db91e9ce77f8ed4892f4da1e4b643b
Smiti Kothari <kotharismiti24@gmail.com>
2024-11-27 00:44:30 +0530

    Updated delete logic

--- code/delete.py
+++ code/delete.py
@@ -1,39 +1,22 @@
-"""
-
-MIT License
-
-Copyright (c) 2021 Dev Kumar
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-
-"""
-
 import helper
+
+# === Documentation of delete.py ===
 
 
 def run(message, bot):
-    global user_list
+    """
+    run(message, bot): This is the main function used to implement the delete feature.
+    It takes 2 arguments for processing - message which is the message from the user, and bot
+    which is the telegram bot object from the main code.py function. It calls helper to get the user
+    history i.e chat ids of all user in the application, and if the user requesting a delete has their
+    data saved in myDollarBot i.e their chat ID has been logged before, run calls the deleteHistory(chat_id):
+    to remove it. Then it ensures this removal is saved in the datastore.
+    """
     chat_id = message.chat.id
     delete_history_text = ""
     user_list = helper.read_json()
-    if (str(chat_id) in user_list):
-        helper.write_json(deleteHistory(chat_id))
+    if str(chat_id) in user_list:
+        helper.write_json(deleteHistory(chat_id,user_list))
         delete_history_text = "History has been deleted!"
     else:
         delete_history_text = "No records there to be deleted. Start adding your expenses to keep track of your spendings!"
@@ -41,8 +24,11 @@ def run(message, bot):
 
 
 # function to delete a record
-def deleteHistory(chat_id):
-    global user_list
-    if (str(chat_id) in user_list):
+def deleteHistory(chat_id,user_list):
+    """
+    deleteHistory(chat_id): It takes 1 argument for processing - chat_id which is the
+    chat_id of the user whose data is to deleted from the user list. It removes this entry from the user list.
+    """
+    if str(chat_id) in user_list:
         del user_list[str(chat_id)]
     return user_list
